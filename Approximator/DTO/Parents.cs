56a3d49788eeeab54ad4376797411c39e5c66b1f
Maciej Moryń <moryn.maciej@protonmail.com>
2025-06-25 17:15:27 +0200

    added Individual class

--- Approximator/DTO/Parents.cs
+++ Approximator/DTO/Parents.cs
@@ -6,11 +6,13 @@ public struct Parents
 {
 	public Individual Father { get; }
 	public Individual Mother { get; }
+	public int PopulationId { get; }
 
 	public Parents(Individual father, Individual mother)
 	{
 		if (father.IsWorseThan(mother)) (father, mother) = (mother, father);
 		this.Father = father;
 		this.Mother = mother;
+		this.PopulationId = father.PopulationId;
 	}
 }
---
+++ Approximator/Genetics/Individual.cs
@@ -0,0 +1,74 @@
+﻿using Approximator.Utils;
+
+namespace Approximator.Genetics;
+
+using DTO;
+
+public class Individual
+{
+	public double DisplayError { get; }
+	private double Error { get; }
+	private double[] Genes { get; }
+	public string Outcome => string.Join(';', this.Genes);
+	public int PopulationId { get; }
+	private (Point, double) Sphere => (Point.FromArray(this.Genes.Take(this.Genes.Length - 1).ToArray()), this.Genes.Last());
+
+	public Individual(Approximator approximator)
+	{
+		this.PopulationId = 0;
+		this.Genes = RandomUtils.RandomArray(approximator.ChromosomeSize);
+		(this.Error, this.DisplayError) = this.EvaluateError(approximator);
+	}
+
+	public Individual(Approximator approximator, Parents parents)
+	{
+		this.PopulationId = parents.PopulationId + 1;
+		this.Genes = Individual.Crossover(parents, approximator);
+		this.Mutate(approximator, this.PopulationId);
+		(this.Error, this.DisplayError) = this.EvaluateError(approximator);
+	}
+
+	private (double, double) EvaluateError(Approximator approximator)
+	{
+		var cloud = approximator.PointCloud;
+		var (center, radius) = this.Sphere;
+		var error = 0.0;
+		foreach (var point in cloud)
+		{
+			error += Math.Abs(point.DistanceTo(center) - radius);
+		}
+
+		return (error, error / cloud.Size);
+	}
+
+	public bool IsBetterThan(Individual other)
+	{
+		return this.Error < other.Error;
+	}
+	
+	public bool IsWorseThan(Individual other)
+	{
+		return this.Error > other.Error;
+	}
+
+	private void Mutate(Approximator approximator, int populationId)
+	{
+		for (var index = 0; index < this.Genes.Length; index++)
+		{
+			var gene = this.Genes[index];
+			this.Genes[index] = RandomUtils.AttemptMutation(gene, approximator.MutationProbability, approximator.BaseThreshold, populationId);
+		}
+	}
+
+	private static double[] Crossover(Parents parents, Approximator approximator)
+	{
+		var genes = new double[approximator.ChromosomeSize];
+		for (var index = 0; index < genes.Length; index++)
+		{
+			var fatherGene = parents.Father.Genes[index];
+			var motherGene = parents.Mother.Genes[index];
+			genes[index] = RandomUtils.SelectWithProbability(fatherGene, motherGene, approximator.PatriarchyLevel);
+		}
+		return genes;
+	}
+}
